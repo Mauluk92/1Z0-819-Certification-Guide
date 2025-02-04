dd0c4b60ccf565f6ab3ab9ab1c4f20ae71fc75f8
Nicola <ncl.rss.92@gmail.com>
2025-02-04 23:33:07 +0100

    c11 - CreatingModularProgramTest

--- src/test/java/it/mauluk92/java/c11/CreatingModularProgramTest.java
+++ src/test/java/it/mauluk92/java/c11/CreatingModularProgramTest.java
@@ -6,14 +6,21 @@ import it.mauluk92.java.testutils.extension.TempDirectoryCallback;
 import it.mauluk92.java.testutils.extension.annotation.CompileClasses;
 import it.mauluk92.java.testutils.extension.annotation.ExecuteJavaProgram;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({JavaCompilerExtension.class, JavaRunnerExtension.class, TempDirectoryCallback.class})
 public class CreatingModularProgramTest {
 
+
+    /**
+     * A module is a group of one or more packages
+     * plus a special file called module-info.java.
+     */
     @Test
-    public void testModule(
+    @DisplayName("Exploring the module")
+    public void exploringTheModule(
             @CompileClasses(classesToCompile = {
                     "internal/module-info.java",
                     "internal/nested/deep/InternalNestedDeepFile.java"
@@ -25,4 +32,45 @@ public class CreatingModularProgramTest {
         Assertions.assertEquals(0, outputCompilation);
         Assertions.assertEquals(0, outputExecution);
     }
+
+    /**
+     * Modules provide another layer of access control
+     * to specify other packages. This stronger form of
+     * encapsulation really does create internal packages
+     */
+    @Test
+    @DisplayName("Better access control")
+    public void betterAccessControl(
+            @CompileClasses(classesToCompile = {
+                    "internal/module-info.java",
+                    "internal/nested/deep/InternalNestedDeepFile.java"
+            }, sourcePath = "c11/creating_modular_program")
+            Integer outputCompilation,
+            @ExecuteJavaProgram(moduleName = "internal.nested.deep/internal.nested.deep.InternalNestedDeepFile")
+            Integer outputExecution
+    ){
+        Assertions.assertEquals(0, outputCompilation);
+        Assertions.assertEquals(0, outputExecution);
+    }
+
+    /**
+     * Modules enforces the uniqueness
+     * of a package provided inside itself
+     */
+    @Test
+    @DisplayName("Unique package enforcement")
+    public void uniquePackageEnforcement(
+            @CompileClasses(classesToCompile = {
+                    "internal/module-info.java",
+                    "internal/nested/deep/InternalNestedDeepFile.java"
+            }, sourcePath = "c11/creating_modular_program")
+            Integer outputCompilation,
+            @ExecuteJavaProgram(moduleName = "internal.nested.deep/internal.nested.deep.InternalNestedDeepFile")
+            Integer outputExecution
+    ){
+        Assertions.assertEquals(0, outputCompilation);
+        Assertions.assertEquals(0, outputExecution);
+    }
+
+
 }
