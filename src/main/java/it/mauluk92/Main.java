53d33e87d852afdffaf1034a86b5d9c4dbc58a9b
Nicola Rossi <Nicola.Rossi@corvallis.it>
2025-02-26 10:41:36 +0100

    fix main with welcome message

--- src/main/java/it/mauluk92/Main.java
+++ src/main/java/it/mauluk92/Main.java
@@ -2,6 +2,6 @@ package it.mauluk92;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Java Certification Guide!");
     }
 }
